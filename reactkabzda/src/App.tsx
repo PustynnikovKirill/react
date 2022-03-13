a4d1935aea0ac26ee1f6db977d3fb084a1370517
Kirill Pustynnikov <kiryuha911@yandex.by>
2022-03-13 17:54:35 +0300

    'banknots'

--- reactkabzda/src/App.tsx
+++ reactkabzda/src/App.tsx
@@ -3,22 +3,47 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    let [a,setA] = useState(1)
-    const conside=()=>{
-       setA(++a)
-        console.log(a)
-    }
-    const zero =()=>{
-     setA(0)
-        console.log (0)
-    }
-  return (
-    <div className="App">
-        <h1>{a}</h1>
-     <button onClick={conside}>number</button>
-     <button onClick={zero}>0</button>
-    </div>
-  );
-}
+    const [money, setMoney] = useState([
+        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+    ])
+        let [filter,setFilter]=useState('All')
+        let filterMoney= money;
+        if  (filter==="RUBLS"){
+            filterMoney = money.filter((filterBanknots)=>filterBanknots.banknots==='RUBLS');
+        }
+        if (filter==="Dollars"){
+            filterMoney = money.filter((filterBanknots)=>filterBanknots.banknots==='Dollars');
+        }
 
-export default App;
+
+    const onClickFilterHandler=(nameButton:string)=>{
+            setFilter(nameButton)
+        }
+        return (
+         <>
+            <ul>
+                {filterMoney.map((objFromMoneyArray, index)=>{
+                return (
+                    <li key={index}>
+                        <span>{objFromMoneyArray.banknots}</span>
+                        <span>{objFromMoneyArray.value}</span>
+                        <span>{objFromMoneyArray.number}</span>
+                    </li>
+                )})}
+            </ul>
+                <div style={{marginLeft:'35px'}}>
+                    <button onClick={()=>onClickFilterHandler('all')}> all </button>
+                    <button onClick={()=>onClickFilterHandler("ruble")}> ruble </button>
+                    <button onClick={()=>onClickFilterHandler('dollars')}> dollars </button>
+                </div>
+            </>
+        )
+}
+    export default App;
